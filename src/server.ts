4a123356725081f4c2cfcd5b1b402da2ca6ac8d9
nizamseu <nizamcse.seu@gmail.com>
2023-10-04 17:24:39 +0600

    db setup Done

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_URL as string);
+
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+    console.log("connected To Db");
+  } catch (error) {
+    console.log("fail to connect DB");
+  }
+}
+
+main();
