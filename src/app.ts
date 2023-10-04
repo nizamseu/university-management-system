70793956a42d4588b5a5da846a645652773298d3
nizamseu <nizamcse.seu@gmail.com>
2023-10-04 17:33:38 +0600

    [Done]: testing Route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
 const app: Application = express();
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// tesing route
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
