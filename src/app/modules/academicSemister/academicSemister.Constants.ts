1a4689fc40eb8e0999e2c4b1b22081b8c427478a
nizamseu <nizamcse.seu@gmail.com>
2023-10-19 14:09:32 +0600

    Semester Validation added

--- src/app/modules/academicSemister/academicSemister.Constants.ts
+++ src/app/modules/academicSemister/academicSemister.Constants.ts
@@ -26,3 +26,9 @@ export const academicSemisterTitle: IAcademicSemisterTitle[] = [
 ];
 
 export const academicSemisterCode: IAcademicSemisterCode[] = ['01', '02', '03'];
+
+export const academicSemesterTitleCodeMapper: { [key: string]: string } = {
+  Autum: '01',
+  Summer: '02',
+  Fall: '03',
+};
--- src/app/modules/academicSemister/academicSemister.service.ts
+++ src/app/modules/academicSemister/academicSemister.service.ts
@@ -1,9 +1,14 @@
+import { APIError } from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemister.Constants';
 import { IAcademicSemister } from './academicSemister.interface';
 import { AcademicSemester } from './academicSemisterModel';
-
+import { StatusCodes } from 'http-status-codes';
 const createSemister = async (
   payload: IAcademicSemister,
 ): Promise<IAcademicSemister> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new APIError(StatusCodes.BAD_REQUEST, 'Invalid Semester');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
