468e5fc43622a6a89c6eb5405a3b9b3b88ea4b49
nizamseu <nizamcse.seu@gmail.com>
2023-10-19 15:04:27 +0600

    handle not found path added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middleware/globarErrorHandler';
 import routers from './app/routes';
-
+import { StatusCodes } from 'http-status-codes';
 // import { APIError } from './errors/ApiError'
 
 const app: Application = express();
@@ -24,6 +24,16 @@ app.use('/api/v1', routers);
 
 //global error handler
 app.use(globalErrorHandler);
+
+// handle not Found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessage: [{ path: req.originalUrl, message: 'API Not Found' }],
+  });
+  next();
+});
 
 // tesing route
 app.get('/', async (req: Request, res: Response, next: NextFunction) => {
